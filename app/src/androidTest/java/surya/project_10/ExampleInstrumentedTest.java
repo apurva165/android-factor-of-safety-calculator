8adb7fe0a53d9444028ce07f9c76d04899433389
Apurva165 <Apurva165@gmail.com>
2017-10-30 22:49:58 -0500

    all reached to final calculation

--- app/src/androidTest/java/surya/project_10/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/surya/project_10/ExampleInstrumentedTest.java
@@ -3,6 +3,7 @@ package surya.project_10;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,6 +24,22 @@ public class ExampleInstrumentedTest {
 
         assertEquals("surya.project_10", appContext.getPackageName());
     }
-
+    @Test
+    public void testtanInverse() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        double degrees = .1818;
+        double radians = Math.toRadians(degrees);
+        String tanuse = Double.toString((Math.toDegrees(Math.atan((.2)/(1.1)))));
+        Log.d("tan calculation", tanuse);
+    }
+    @Test
+    public void checkXX() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        double xx = (Math.cos( Math.toRadians(10.3) +  Math.toRadians(35))) / (Math.cos( Math.toRadians(10.3)));
+        Log.d("fuck2", Double.toString(xx));
+        System.out.println("fuck you");
+    }
 
 }
--- app/src/main/java/surya/project_10/home_page.java
+++ app/src/main/java/surya/project_10/home_page.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.util.Log;
 
 public class home_page extends AppCompatActivity {
 
